227b5e74802e50f29aab35dcb998f926a2c0bebd
haksonglee <blank@nate.com>
2020-03-19 11:21:00 +0900

    111

--- router/test.js
+++ router/test.js
@@ -6,7 +6,7 @@ let yedate;
 
 router.post('/', function(req, res) {
   //let params = req.body.action.params
-  deptname = '피부과'
+  deptname = '신경과'
 
   let drlist_script = require('./call_drlist');
   let drlist_bodydata = JSON.stringify(drlist_script.call_drlist(deptname, drname, yedate, 'dept'));
